2efb6ae055b0866e8e698ff5d3d368e8b5e40187
Arify <justoctober2011@gmail.com>
2023-08-01 11:45:48 +0100

    updated home_screen
    added more lines of code

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -9,18 +9,42 @@ class HomeScreen extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(
           top: MediaQuery.of(context).padding.top, left: 25, right: 15),
-      child: Row(
+      child: Column(
         children: [
-          Text(
-            'Howdy, What are You\n Looking For 👁️',
-            style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-          ),
-          Container(
-            child: SvgPicture.asset(
-              'assets/icons/cart.svg', 
-              width:20
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Howdy, What are You\n Looking For 👁️',
+                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
               ),
-            )
+              Container(
+                child: SvgPicture.asset('assets/icons/cart.svg', width: 20),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Search for Products',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  prefix: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/search.svg',
+                      width: 20,
+                    ),
+                  )),
+            ),
+          )
         ],
       ),
     );
